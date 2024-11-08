0fa7a5e93beb6de15e96cd89f0aa23ae2db0abc9
ayushmeharkure13 <148432915+ayushmeharkure13@users.noreply.github.com>
2024-11-08 17:19:29 +0000

    dtypes completed

---
+++ 01_basics/02_dtypes.js
@@ -0,0 +1,32 @@
+"use strict"    //treats all code as the newer version of javascript
+// the javascript engine is hidden inside the browser
+let name_me = "aayush"
+let age = 33
+let is_loggedIn = true
+
+/*types of all dtypes 
+1. number 
+2. string ==> " " , ' '
+3. boolean ==> true/false
+4. null ==> standalone value
+5. undefined 
+6. symbol ==> unique determination
+*/
+
+console.log(typeof name_me)
+console.log(typeof age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//node 01_basics/02_dtypes.js
